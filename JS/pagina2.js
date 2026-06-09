4056e94ac83d765cccf400f992efa479c93a4d74
João Pedro de Oliveira Batista <joaop161008@gmail.com>
2026-06-09 14:37:24 -0300

    Atualizei

--- JS/pagina2.js
+++ JS/pagina2.js
@@ -1,6 +1,12 @@
 function iniciamodal(modalid){
     var modal = window.document.getElementById(modalid)
-    modal.classList.add('mostrar')
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+      if(e.target.id == modalid){
+        modal.classList.remove('mostrar')
+      }
+        
+    });
 }
 
 var n9 = document.querySelector('.n9')
